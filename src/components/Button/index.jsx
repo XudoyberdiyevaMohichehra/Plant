2ff91059301db229c10f1daaa32982647162f3a2
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-06-13 22:22:33 +0500

    button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./style";
+
+const NormalButton = ({ children, ...rest }) => {
+    return <Container {...rest}>{children}</Container>;
+};
+
+export default NormalButton;
